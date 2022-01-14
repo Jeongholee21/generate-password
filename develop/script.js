7b75fa9e3b19b7e63700debb99462e4fd46175aa
jeongholee <jeongholee@hotmail.com>
2022-01-14 12:48:26 -0800

    fixed bugs

--- develop/script.js
+++ develop/script.js
@@ -4,65 +4,69 @@
 var upperCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowerCharacters = "abcdefghijklmnopqrstuvwxyz";
 var numericCharacters = "0123456789";
-var specialCharacters = "`~!@#$%^&*()-_=+{[]}|;:',<.>/? ";
+var specialCharacters = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
 // Prompt How long passward length will be
-// if there is no input number? no passwordLength data
-
 function generatePassword() {
+    var passwordCharacters = "";
     var passwordLength = prompt("How many characters want to make for the password? Choose between 8 to 128");
 
     if (passwordLength >= 8 && passwordLength <= 128) {
-        var passwordCharacters ="";
-        
+
         // Asking about which characters in your password
         // UPEER CASE
-        var upperCase = prompt(`Do you want to contain Upper Characters in your password? Please Enter: "YES" or "NO"`);
-        upperCase = upperCase.toUpperCase();
-        if (upperCase === "YES") {
+        var upperCase = prompt(`Do you want to contain Upper Characters in your password? Please Enter: 1."YES" or 2."NO"`);
+
+        if (upperCase === "YES" || upperCase === "1") {
             passwordCharacters += upperCharacters;
         }
         // LOWER CASE
-        var lowerCase = prompt(`Do you want to contain Lower Characters in your password? Please Enter: "YES" or "NO"`);
-        lowerCase = lowerCase.toUpperCase();
-        if (upperCase === "YES") {
+        var lowerCase = prompt(`Do you want to contain Lower Characters in your password? Please Enter: 1."YES" or 2."NO"`);
+
+        if (lowerCase === "YES" || lowerCase === "1") {
             passwordCharacters += lowerCharacters;
         }
         // NUMERIC CASE
-        var numericCase = prompt(`Do you want to contain Numeric Characters in your password? Please Enter: "YES" or "NO"`);
-        numericCase = numericCase.toUpperCase();
-        if (numericCase === "YES") {
+        var numericCase = prompt(`Do you want to contain Numeric Characters in your password? Please Enter: 1."YES" or 2."NO"`);
+
+        if (numericCase === "YES" || numericCase === "1") {
             passwordCharacters += numericCharacters;
         }
         // SPECIAL CASE
-        var specialCase = prompt(`Do you want to contain Special Characters in your password? Please Enter: "YES" or "NO"`);
-        specialCase = specialCase.toUpperCase();
-        if (specialCase === "YES") {
+        var specialCase = prompt(`Do you want to contain Special Characters in your password? Please Enter: 1."YES" or 2."NO"`);
+
+        if (specialCase === "YES" || specialCase === "1") {
             passwordCharacters += specialCharacters;
         }
 
-        //if didn't choose any characters?
-        //need pass word generation code using Math.random()
+        // if didn't choose any characters, make alert and return to fuction.
+        if (passwordCharacters.length === 0) {
+            alert("you didn't contain any characters, please choose at least one agian");
+            return generatePassword()
+        }
+
         var newPassword = "";
 
         for (i = 0; i < passwordLength; i++) {
-            newPassword += passwordCharacters[Math.floor(Math.random()*passwordCharacters.length-1)];
-        };
+            newPassword += passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+        }
+        return newPassword;
+
     } else {
-        alert("Please enter a number between 8 to 128!");
-    };
-    return newPassword;
- 
+        alert("Please enter a number between 8 to 128! Try Again");
+        return generatePassword()
+    }
 };
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
